7120671adb4ecd3c624564780cf06bd77a0c147d
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-21 21:55:49 -0400

    a function has been added

--- href_values_finder.py
+++ href_values_finder.py
@@ -30,20 +30,26 @@ clever Python program to solve the program.
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
-
+import re
+links_lst = []
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+def last_name_extractor(x):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
 
+# iterations = input('Please enter the number of iterations or how many times you would like to repeat this process: ')
 # Retrieve all of the anchor tags
-counts = []
-tags = soup('a')
-for tag in tags:
-    counts.append(tag.get('href'))
-print(counts)
-    
+    tags = soup('a')
+    for tag in tags:
+        links_lst.append(tag.get('href'))
+    link_desired = links_lst[2]
+
+#retrive the 3rd link
+    return(link_desired)                                         # count numbers start at 0
+linking = last_name_extractor(url)
+print('The result is: ', linking)
